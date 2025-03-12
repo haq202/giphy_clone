4286473264c86afe1d7e08d8cd9dde003d75c00d
Hoang Anh Quan <quanha2@viettel.com.vn>
2025-03-12 15:55:11 +0700

    Code layout home

--- giphy/src/app/app.component.ts
+++ giphy/src/app/app.component.ts
@@ -2,11 +2,18 @@ import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoaderService } from '@core/services/loader.service';
 import { LoaderComponent } from './layouts/loader/loader.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoaderComponent],
+  imports: [
+    RouterOutlet,
+    LoaderComponent,
+    HeaderComponent,
+    MainLayoutComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
