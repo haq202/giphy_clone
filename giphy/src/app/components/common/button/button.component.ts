735e012670275b5a11c77bd98e2a7f3d10db0d37
quanha2 <quanha2@viettel.com.vn>
2025-03-15 17:51:22 +0700

    Dev header

--- giphy/src/app/components/common/button/button.component.ts
+++ giphy/src/app/components/common/button/button.component.ts
@@ -1,11 +1,38 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  output,
+} from '@angular/core';
+import { ButtonSeverityType, ButtonType } from '@core/models/layout.model';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ButtonComponent {}
+export class ButtonComponent {
+  label = input<string>('');
+  href = input<string>('');
+  icon = input<string>('');
+  severity = input<ButtonSeverityType>('primary');
+  disabled = input<boolean>(false);
+  type = input<ButtonType>('button');
+  buttonClick = output<MouseEvent>();
+  rounded = input<boolean>(false);
+  outlined = input<boolean>(false);
+  link = input<boolean>(false);
+  text = input<boolean>(false);
+  styleClass = input<string>('');
+  loading = input<boolean>(false);
+  height = input<string>('unset');
+  width = input<string>('unset');
+
+  onClick(event: MouseEvent) {
+    this.buttonClick.emit(event);
+  }
+}
--- giphy/src/app/layouts/header/header.component.ts
+++ giphy/src/app/layouts/header/header.component.ts
@@ -1,11 +1,23 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ButtonComponent } from '../../components/common/button/button.component';
+import { INavbarItem } from '@core/models/layout.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  navItems: INavbarItem[] = [
+    { label: 'Reactions', href: '/reactions' },
+    { label: 'Entertainment', href: '/entertainment' },
+    { label: 'Sports', href: '/sports' },
+    { label: 'Stickers', href: '/stickers' },
+    { label: 'Artists', href: '/artists' },
+    { icon: 'pi pi-ellipsis-v', dropdown: [] },
+  ];
+}
