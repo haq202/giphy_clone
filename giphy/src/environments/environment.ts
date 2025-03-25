fa0338dd6db61a1de85ce9f0d90ff0edde9927e9
quanha2 <quanha2@viettel.com.vn>
2025-03-25 22:30:04 +0700

    Hide firebase config

--- giphy/src/environments/environment.ts
+++ giphy/src/environments/environment.ts
@@ -14,13 +14,6 @@ export const environment = {
     },
   },
   firebaseConfig: {
-    apiKey: 'AIzaSyAYsRwOxq7ysUaTFTKaM7k1Jt2EBojVOqk',
-    authDomain: 'giphy-e8c54.firebaseapp.com',
-    databaseURL: 'https://giphy-e8c54-default-rtdb.firebaseio.com',
-    projectId: 'giphy-e8c54',
-    storageBucket: 'giphy-e8c54.firebasestorage.app',
-    messagingSenderId: '206538829301',
-    appId: '1:206538829301:web:6e02f68b2ebf7f3719c4bc',
-    measurementId: 'G-GJMMQG5RQJ',
+
   },
 };
